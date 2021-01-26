294bb480560bae4481fb2db67f0a42b9021d0676
Marko <vukoticmarko99@gmail.com>
2021-01-27 00:18:48 +0100

    Added prikaz manifestacije

--- static/front/prikazManifestacije.js
+++ static/front/prikazManifestacije.js
@@ -1,21 +1,24 @@
 Vue.component("prikazmanifestacije",{
     data: function(){
         return{
-          manifestacija:null
+          m:""
         }
     },
 
     template:`
-        <img :src="'../images/'+m.slika+'.jpg'" width = "300px" heigth = "300">
-        <div class="card-body">
-            <p class="card-text">Naziv: {{m.naziv}}<br/>Datum: {{m.datum.date.day}}.{{m.datum.date.month}}.{{m.datum.date.year}}
-            u {{m.datum.time.hour}}:{{m.datum.time.minute}}<br/>
-            Lokacija: {{m.lokacija.adresa.ulica}} {{m.lokacija.adresa.broj}}
-            {{m.lokacija.adresa.mesto}} {{m.lokacija.adresa.postBroj}}<br/>Broj mesta: {{m.brojMesta}}<br/>
-            Cena karte je: {{m.cena}}</p>
+        <div>
+            <img :src="'../images/'+m.slika+'.jpg'" width = "300px" heigth = "300">
+            <div class="card-body">
+                <p class="card-text">Naziv: {{m.naziv}}<br/>Datum: {{m.datum.date.day}}.{{m.datum.date.month}}.{{m.datum.date.year}}
+                u {{m.datum.time.hour}}:{{m.datum.time.minute}}<br/>
+                Lokacija: {{m.lokacija.adresa.ulica}} {{m.lokacija.adresa.broj}}
+                {{m.lokacija.adresa.mesto}} {{m.lokacija.adresa.postBroj}}<br/>Broj mesta: {{m.brojMesta}}<br/>
+                Cena karte je: {{m.cena}}</p>
+            </div>
         </div>
     `,
     mounted(){
-        this.manifestacija=JSON.parse(localStorage.getItem('manif'))
-    },
+        this.m=JSON.parse(localStorage.getItem('manif'))
+        console.log(this.m)
+    }
 })
